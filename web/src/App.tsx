c6db412fcc7ae9cc988c8e0c3b3a5a3da0eef96a
guilhermedjr <guilhermedjrdjrjan@gmail.com>
2020-10-14 13:25:15 -0300

    Integrating website to API

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './styles/global.css';
-import './styles/pages/landing.css'
 import Routes from './routes';
+import 'Leaflet/dist/leaflet.css';
 
 function App(){
   return <Routes/>
